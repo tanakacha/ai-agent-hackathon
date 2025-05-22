2bb3ac30ade7fcc80cb45a465b2d3b25b1862cb0
Melonps <kake.kake0311@gmail.com>
2025-05-22 20:45:14 +0900

    [FEAT] java applicationのベースを追加

---
+++ java_backend/src/main/java/com/example/ai_hackathon/AiHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiHackathonApplication.class, args);
+	}
+
+}
---
+++ java_backend/src/main/java/com/example/ai_hackathon/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.ai_hackathon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, ";
+    }
+}
