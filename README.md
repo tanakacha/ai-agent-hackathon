78cdd1ed15817034168955e67bee7bfc83fb730e
Melonps <kake.kake0311@gmail.com>
2025-05-24 21:48:50 +0900

    [FIX] 設定ファイルを編集

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ai-agent-hackathon
+
 第2回 AI Agent Hackathon with Google Cloud by Zenn
+
+## Java_Backendの立ち上げ方
+
+gradleのバージョン確認
+
+```sh
+gradle --version
+```
+
+開発用サーバーの立ち上げ(ai-agent-hackathon/java_backend)
+
+```sh
+./gradlew bootRun
+```
--- java_backend/src/main/java/com/example/ai_hackathon/AiHackathonApplication.java
+++ java_backend/src/main/java/com/example/ai_hackathon/AiHackathonApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiHackathonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AiHackathonApplication.class, args);
 	}
-
 }
