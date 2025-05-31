7653df77b1bce5fa6adff82afe86088861edada8
Melonps <kake.kake0311@gmail.com>
2025-05-31 09:36:56 +0900

    [FEAT] node modelを追加

---
+++ java_backend/src/main/java/com/example/model/Node.java
@@ -0,0 +1,22 @@
+package com.example.model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoadMap {
+	private String id;
+	private String user_id;
+	private String title;
+	private String objective;
+	private Date deadline;
+	private Date created_at;
+	private Date updated_at;
+}
