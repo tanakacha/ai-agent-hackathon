87719e275304b36f70452268300610cdf781dfa6
shotarai <second.arasho@outlook.jp>
2025-06-02 22:30:02 +0200

    fix: dtoディレクトリに戻した

--- java_backend/src/main/java/com/example/ai_hackathon/controller/RoadmapController.java
+++ java_backend/src/main/java/com/example/ai_hackathon/controller/RoadmapController.java
@@ -1,8 +1,8 @@
 package com.example.ai_hackathon.controller;
 
 import com.example.service.RoadmapGenerationService;
-import com.example.model.RoadmapRequest;
-import com.example.model.RoadmapResponse;
+import com.example.dto.RoadmapRequest;
+import com.example.dto.RoadmapResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
