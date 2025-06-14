0a92c170056356de467fd344257e2d9565a1e709
Melonps <kake.kake0311@gmail.com>
2025-06-14 18:52:46 +0900

    [FEAT] repositoryとmanagerの追加

---
+++ flutter_client/lib/common/view_model/roadmap_manager.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_client/common/interface/roadmap_repository.dart';
+import 'package:flutter_client/common/interface/roadmap_repository_repository.dart';
+import 'package:flutter_client/common/model/node.dart';
+import 'package:flutter_client/common/model/road_map.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'roadmap_manager.g.dart';
+
+abstract class RoadmapManagerInterface {
+  RoadmapManagerInterface();
+  Future<Map<int, Node>> getAllNodes(String userId, String mapId);
+  Future<Map<int, Node>> exploreNodes({
+    required List<Node> selectedNodeList,
+    required String mapId,
+    required String objective,
+    required String profile,
+  });
+  Future<Map<int, Node>> adjustNodes(RoadMap roadMap);
+}
+
+@riverpod
+RoadmapManagerInterface roadmapManager(RoadmapManagerRef ref) =>
+    _RoadmapManager(
+      roadmapRepository: ref.watch(roadmapRepositoryProvider),
+    );
+
+class _RoadmapManager extends RoadmapManagerInterface {
+  _RoadmapManager({
+    required this.roadmapRepository,
+  });
+
+  final RoadmapRepositoryInterface roadmapRepository;
+
+  @override
+  Future<Map<int, Node>> getAllNodes(String userId, String mapId) async =>
+      roadmapRepository.getAllNodes(userId, mapId);
+
+  @override
+  Future<Map<int, Node>> exploreNodes({
+    required List<Node> selectedNodeList,
+    required String mapId,
+    required String objective,
+    required String profile,
+  }) async =>
+      roadmapRepository.exploreNodes(
+        selectedNodeList: selectedNodeList,
+        mapId: mapId,
+        objective: objective,
+        profile: profile,
+      );
+
+  @override
+  Future<Map<int, Node>> adjustNodes(RoadMap roadMap) async =>
+      roadmapRepository.adjustNodes(roadMap);
+}
